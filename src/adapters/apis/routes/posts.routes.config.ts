7e5c3cd42e08fff4765a1fea0b9daf6379c2511f
MonicaIacy <monicamendes81@yahoo.com>
2022-10-24 18:55:57 -0300

    feat: updating posts routes and app.ts

--- src/adapters/apis/routes/posts.routes.config.ts
+++ src/adapters/apis/routes/posts.routes.config.ts
@@ -1,35 +1,30 @@
-// import { CommonRoutesConfig } from "./common.routes.config";
-// import PostsController from "../controllers/posts.controller";
-// import PostsMiddleware from "../middlewares/posts.middleware";
-// import express from "express";
+import { CommonRoutesConfig } from "./common.routes.config";
+import PostsController from "../controllers/posts.controller";
+import PostsMiddleware from "../middlewares/posts.middleware";
+import express from "express";
 
-// export class PostsRoutes extends CommonRoutesConfig {
-//     constructor(app: express.Application) {
-//         super(app, 'PostsRoutes');
-//     }
+export class PostsRoutes extends CommonRoutesConfig {
+  constructor(app: express.Application) {
+    super(app, "PostsRoutes");
+  }
 
-//     configureRoutes(): express.Application {
-//         this.app.route(`/posts`)
-//             .get(PostsController.listPosts)
-//             .post(
-//                 PostsMiddleware.validateRequiredPostBodyFields,
-//                 PostsMiddleware.validateUserExists,
-//                 PostsMiddleware.validatePostRepeated,
-//                 PostsController.createPost,
-//             );
+  configureRoutes(): express.Application {
+    this.app
+      .route(`/posts`)
+      .get(PostsController.listPosts)
+      .post(
+        PostsMiddleware.validateRequiredPostBodyFields,
+        PostsMiddleware.validatePostExists,
+        PostsController.createPost
+      );
 
-//             this.app.route(`/posts/:postId`)
-//                         .all(PostsMiddleware.validatePostExists)
-//                         .get(PostsController.getPostById)
-//                         .put(
-//                             PostsMiddleware.validateRequiredPostBodyFields,
-//                             PostsController.updatePost
-//                         )//TODO conferir delete
-//                         .delete(
-//                             PostsMiddleware.validatePostExists,
-//                             PostsController.removePost
-//                             );
+    this.app
+      .route(`/posts/:postId`)
+      .all(PostsMiddleware.validatePostExists)
+      .get(PostsController.getPostById)
+      .put(PostsMiddleware.validateRequiredPostBodyFields, PostsController.updatePost) //TODO conferir delete
+      .delete(PostsMiddleware.validatePostExists, PostsController.removePost);
 
-//         return this.app;
-//     }
-// }
+    return this.app;
+  }
+}
