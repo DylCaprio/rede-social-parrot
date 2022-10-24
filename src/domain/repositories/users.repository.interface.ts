91181c74333cacbd7a468e078191f4a6206511de
MonicaIacy <monicamendes81@yahoo.com>
2022-10-23 21:10:53 -0300

    feat: creating domain repositories

---
+++ src/domain/repositories/users.repository.interface.ts
@@ -0,0 +1,9 @@
+import { IUserEntity } from "../entities/users/user.entity";
+
+export interface IUsersRepository {
+  readById(resourceId: number): Promise<IUserEntity | undefined>;
+  create(resource: IUserEntity): Promise<IUserEntity>;
+  deleteById(resourceId: number): Promise<void>;
+  list(): Promise<IUserEntity[]>;
+  updateById(resource: IUserEntity): Promise<IUserEntity | undefined>;
+}
