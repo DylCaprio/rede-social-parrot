a2f67380b34bb82793062dddfe88b94c876f56d6
MonicaIacy <monicamendes81@yahoo.com>
2022-10-24 18:08:05 -0300

    feat: implementing user constants

---
+++ src/infrastructure/config/constants.config.ts
@@ -0,0 +1,12 @@
+export default {
+  USERS: {
+    MESSAGES: {
+      ERROR: {
+        INVALID_EMAIL: `Você deve enviar um email válido.`,
+        VOID_BODY: `Você deve preencher os campos de nome, email, apartamento e senha.`,
+        USER_NOT_EXISTS: `Usuário {USER_ID} não existe`,
+        USER_ALREADY_EXISTS: `Usuário {USER_ID} já existe`,
+      },
+    },
+  },
+};
